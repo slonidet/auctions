d65c6e0574a382930cd74a92da344267c3dd7a82
dmitry <dmitrysulin@gmail.com>
2018-04-26 10:43:23 +0300

    Create Auction view

--- auctions/auctions/urls.py
+++ auctions/auctions/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from trades.views import AuctionViewSet
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auctions/', AuctionViewSet)
 ]
--- auctions/trades/views.py
+++ auctions/trades/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from trades.models import Auction
+from trades.serializers import AuctionSerializer
+
+
+class AuctionViewSet(ReadOnlyModelViewSet):
+    queryset = Auction.objects.all()
+    serializer_class = AuctionSerializer
