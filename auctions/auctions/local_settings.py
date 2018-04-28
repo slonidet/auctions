cc04cf001257c71aeb113786f6acb0875edf4894
dmitry <dmitrysulin@gmail.com>
2018-04-28 12:30:24 +0300

    SMTP settings

--- auctions/auctions/local_settings.py
+++ auctions/auctions/local_settings.py
@@ -15,3 +15,14 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+
+
+# Email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'dmitrysulin@gmail.com'
+EMAIL_HOST_PASSWORD = 'Acetonio26'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
