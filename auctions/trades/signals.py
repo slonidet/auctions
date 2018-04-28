f3290e38970cb8354b51d3969f0efc5d8bebdc39
dmitry <dmitrysulin@gmail.com>
2018-04-28 18:30:14 +0300

    Create price changing email signal

--- auctions/trades/signals.py
+++ auctions/trades/signals.py
@@ -3,19 +3,34 @@ from django.core.mail import EmailMessage
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from trades.models import Auction
+from trades.models import Auction, Bid
 
 
 @receiver(post_save, sender=Auction)
-def auction_mail(sender, instance, created, **kwargs):
+def auction_creation_mail(sender, instance, created, **kwargs):
     """
-    Send notifications about new auction
+    Send notifications about creating new auction
     """
     if created:
         title = 'New auction is avalable'
         body = 'You can take a part in this auction: {title}'.format(
             title=instance.title
         )
-        users = User.objects.all()
+        users = User.objects.filter(is_active=True)
+        email = EmailMessage(title, body, to=[user.email for user in users])
+        email.send()
+
+
+@receiver(post_save, sender=Bid)
+def auction_notifications_mail(sender, instance, created, **kwargs):
+    """
+    Send notifications about the auction
+    """
+    if created:
+        auction = Auction.objects.get(bid=instance)
+        users = User.objects.filter(is_active=True, auction=auction)
+        title = "The price on {ttl} was changed".format(ttl=auction.title)
+        body = "The price on {ttl} was changed to {am}".format(
+            ttl=auction.title, am=instance.amount)
         email = EmailMessage(title, body, to=[user.email for user in users])
         email.send()
