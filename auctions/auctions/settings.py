5295c072f4eec94d261be09afda460f58e16bda4
dmitry <dmitrysulin@gmail.com>
2018-04-29 14:42:42 +0300

    Create send expiration email celery task

--- auctions/auctions/settings.py
+++ auctions/auctions/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from celery.schedules import crontab
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -133,4 +135,16 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
     )
+}
+
+
+# Celery configuration
+# http://docs.celeryproject.org/
+
+CELERY_BROKER_URL = ""
+CELERY_BEAT_SCHEDULE = {
+    'alert_users': {
+        'task': 'notificate_expired_auction',
+        'schedule': crontab(minute=0, hour=0),
+    }
 }
---
+++ auctions/trades/tasks.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+
+from auctions.celery import app
+from trades.models import Auction, Bid
+
+
+@app.task(name='notificate_expired_auction')
+def notificate_expired_auction():
+    auctions = Auction.objects.all()
+    for auction in auctions:
+        users = User.objects.filter(auctions=auction)
+        winner_bid = Bid.objects.get(
+            auction=auction, amount=auction.current_bid)
+        winner = winner_bid.user
+
+        title = 'Auction {ttl} just expired'.format(ttl=auction.title)
+        body = 'Auction {ttl} just expired. {usr} won'.format(
+            ttl=auction.title, usr=winner
+        )
+        users = users.exclude(winner)
+        email = EmailMessage(title, body, to=[user.email for user in users])
+        if not auctions.is_active:
+            email.send()
